a118f8d245caeffe0fa2144e309392cdf2678918
Anca Calugar <anca.calugar@softvision.ro>
2018-01-03 13:35:48 +0200

    added dummy data

--- StickyScores/AJGameCollectionViewController.swift
+++ StickyScores/AJGameCollectionViewController.swift
@@ -11,22 +11,29 @@ import UIKit
 private let reuseIdentifier = "ContentCell"
 
 class AJGameCollectionViewController: UICollectionViewController {
+    
+    var inputData = [Dictionary<String, Any>]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        createInputData()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func createInputData() {
+        for playerIndex in 1 ..< 8 {
+            var scores = [Int]()
+            for scoreIndex in 1 ..< 15 {
+                scores.append(scoreIndex)
+            }
+            inputData.append(["name":"Pl\(playerIndex)", "scores":scores])
+        }
     }
 
     /*
@@ -42,14 +49,13 @@ class AJGameCollectionViewController: UICollectionViewController {
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 15
+        return (inputData[0]["scores"] as! Array<Int>).count
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 8
+        return inputData.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
